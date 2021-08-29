c67be2794af5b359d2ae807839d83bf480f50ce2
Laurent Franceschetti <laurent.franceschetti@settlenext.com>
2021-08-29 17:18:23 +0200

    Amend the `update_pypi.sh` script to create tags in github

--- update_pypi.sh
+++ update_pypi.sh
@@ -1,9 +1,34 @@
+# -------------------------------------------------------------
 # update the package on pypi
-echo "Cleaning up..."
+#
+# Tip: if you don't want to retype pypi's username every time
+#      define it as an environement variable (TWINE_USERNAME)
+# -------------------------------------------------------------
+function warn {
+    GREEN='\033[0;32m'
+    NORMAL='\033[0m'
+    echo -e "${GREEN}$1${NORMAL}"
+}
+
+setup="python3 setup.py"
+package_name=$($setup --name)
+package_version=v$($setup --version) # add a 'v' in front (git convention) 
+
+warn "UPDATE PACKAGE $package_name ($package_version) ON PYPI:"
+warn "Cleaning up..."
 rm -rf dist
 rm -rf build
-echo "Recreating wheels..."
-python3 setup.py sdist bdist_wheel
-echo "---"
-echo "UPLOAD..."
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+warn "Recreating wheels..."
+$setup sdist bdist_wheel  1>/dev/null
+# update version (just in case):
+package_version=v$($setup --version) # add a 'v' in front (git convention) 
+warn "---"
+warn "Upload to Pypi..."
+if twine upload --repository-url https://upload.pypi.org/legacy/ dist/* ; then
+    warn "... create tag $package_version, and push to remote git repo..."
+    git tag $package_version
+    git push --tags
+    warn "Done ($package_version)!"
+else
+    warn "Failed ($package_version)!"
+fi
